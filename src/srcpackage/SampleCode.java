b27630bcf3f52ed45a69282a5cb027832c21760d
Arpan Basak <arpan22082003@gmail.com>
2026-03-29 20:50:30 +0530

    Third commit

--- src/srcpackage/SampleCode.java
+++ src/srcpackage/SampleCode.java
@@ -3,7 +3,7 @@ package srcpackage;
 public class SampleCode {
 
 	public static void main(String[] args) {
-		System.out.peintln("Arpan Basak");
+		System.out.println("Arpan Basak");
 		System.out.println("23MIC0064");
 		System.out.print("Second  Commit");
 
