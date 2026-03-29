6a8631aa6e64d0978a72febe8abd36ea2b5c6eaa
Arpan Basak <arpan22082003@gmail.com>
2026-03-29 16:46:13 +0530

    First Commit

---
+++ src/srcpackage/SampleCode.java
@@ -0,0 +1,12 @@
+package srcpackage;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+		System.out.peintln("Arpan Basak");
+		System.out.println("23MIC0064");
+		System.out.print("First Commit");
+
+	}
+
+}
